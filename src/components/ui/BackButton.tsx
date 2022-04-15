9b0038206f0eee8aeb5192b1fec490e9a126fcb2
omsrg <siregar.om@gmail.com>
2022-04-16 04:55:01 +0700

    remove Link and add navigate

--- src/components/ui/BackButton.tsx
+++ src/components/ui/BackButton.tsx
@@ -1,16 +1,18 @@
-import { Link } from 'react-router-dom';
+import { useNavigate } from 'react-router-dom';
 import { BiArrowBack } from 'react-icons/bi';
 
 const BackButton = () => {
+	const navigate = useNavigate();
 	return (
-		<Link to='/'>
-			<button className='flex items-center rounded-sm border border-dark-200 py-1 px-2 transition-colors hover:bg-dark-500'>
-				<span className='text-base'>
-					<BiArrowBack />
-				</span>
-				<span className='mx-1'>Back</span>
-			</button>
-		</Link>
+		<button
+			className='flex items-center rounded-sm border border-dark-200 py-1 px-2 transition-colors hover:bg-dark-500'
+			onClick={() => navigate(-1)}
+		>
+			<span className='text-base'>
+				<BiArrowBack />
+			</span>
+			<span className='mx-1'>Back</span>
+		</button>
 	);
 };
 export default BackButton;
