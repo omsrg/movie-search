f2703b02099808372868c90a726f15a8780e0118
omsrg <siregar.om@gmail.com>
2022-04-16 03:34:03 +0700

    add line break

--- src/components/MovieDetail/MovieInfo/PlotSection/index.tsx
+++ src/components/MovieDetail/MovieInfo/PlotSection/index.tsx
@@ -13,6 +13,7 @@ const PlotSection: React.VFC<Props> = ({ plot }) => {
 	return (
 		<p className='mt-5 text-base'>
 			{isReadMore ? plot?.slice(0, 150) : plot}
+			{!isReadMore && <br />}
 
 			{plot?.length > 150 && (
 				<span className='text-semibold cursor-pointer text-amber-400' onClick={toggleReadMore}>
